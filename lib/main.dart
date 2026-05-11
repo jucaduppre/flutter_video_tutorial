5ad905c4ddd93445c05d1aeebc7e39db57af6b08
Juca <jucanduppre@gmail.com>
2026-05-11 01:42:07 -0300

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -7,60 +7,20 @@ void main() {
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
+  
   @override
   Widget build(BuildContext context){
+  List names = ["juquinha", "isabele", "maria", "joao", "pedro"]; 
   return MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold( 
-      backgroundColor: Colors.deepPurple[200],
-      appBar: AppBar(
-        title: const Text('juquinhagostoso'
-        , style: TextStyle(
-          color: Color.fromARGB(255, 226, 214, 42),
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-        ),
-        backgroundColor: const Color.fromARGB(255, 230, 18, 184),
-        elevation: 0, 
-        leading: Icon(
-          Icons.menu,
-          color: const Color.fromARGB(255, 59, 157, 238),
-        ),
-        actions: [
-          IconButton(
-            onPressed: (){},
-            icon: Icon(Icons.logout),
-            color: const Color.fromARGB(255, 231, 5, 212),
-          ),
-        ],
-
-        ),
-    
-      body: Column(
-        children: [
-        // 1st box
-        Container(
-          height: 200,
-          width: 200,
-          color: const Color.fromARGB(255, 63, 2, 119),
-        ),
-
-        // 2nd box
-        Container(
-          height: 200,
-          width: 200,
-          color: const Color.fromARGB(255, 165, 43, 247),
-        ),
-
-
-        // 3rd box
-        Container(
-          height: 200,
-          width: 200,
-          color: const Color.fromARGB(255, 148, 12, 153),
-        ),
-        ],
+      body: ListView.builder(
+        itemCount: names.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(names[index]),
+          );
+        }
       )
     ),
    );
