fe00d48e80e627a108a3cddfc95e17fada8e3a14
Juca <jucanduppre@gmail.com>
2026-05-11 01:13:20 -0300

    Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,30 @@ class MyApp extends StatelessWidget{
     debugShowCheckedModeBanner: false,
     home: Scaffold( 
       backgroundColor: Colors.deepPurple[200],
+      appBar: AppBar(
+        title: const Text('juquinhagostoso'
+        , style: TextStyle(
+          color: Color.fromARGB(255, 226, 214, 42),
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 230, 18, 184),
+        elevation: 0, 
+        leading: Icon(
+          Icons.menu,
+          color: const Color.fromARGB(255, 59, 157, 238),
+        ),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.logout),
+            color: const Color.fromARGB(255, 231, 5, 212),
+          ),
+        ],
+
+        ),
+    
       body: Center(
         child: Container(
           height: 300,
