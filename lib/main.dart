2af7ed52c1b6274e19588b7d1fa9cf25dbbcb00c
Juca <jucanduppre@gmail.com>
2026-05-11 10:11:02 -0300

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,26 @@ class MyApp extends StatelessWidget{
   return MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold( 
-      body: GridView.builder(itemCount:64, gridDelegate: const 
-      SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8), 
-      itemBuilder: (context,index)  => Container(
-        color:Colors.black,
-        margin: EdgeInsets.all(2),
-      )
+      body: Stack(
+        children: [
+          Container(
+            height: 300,
+            width: 300,
+            color: const Color.fromARGB(255, 248, 57, 251),
+          ),
+          Container(
+            color: const Color.fromARGB(255, 138, 4, 138),
+            width: 200,
+            height: 200,
+          ),
+
+          Container(
+            width: 100,
+            height: 100,
+            color: const Color.fromARGB(80, 120, 0, 114),
+            )
+        ],
+
       )
     ),
    );
